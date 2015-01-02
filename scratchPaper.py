4f0436a32d02d906a1a401b15675b84178ad97cb
Harby <harby@harby-VirtualBox.(none)>
2015-01-01 23:20:38 -0500

    Committing a new 'scratch paper' python playground for testing/experimenting. added custom Vec2d class

---
+++ scratchPaper.py
@@ -0,0 +1,12 @@
+from basicblock import basicblock
+from pprint import pprint
+
+basicblockTest = basicblock(0,0,64,64)
+pprint("example block x is: " + str(basicblockTest.x))
+pprint("example block y is: " + str(basicblockTest.y))
+pprint("example block width is: " + str(basicblockTest.width))
+pprint("example blcok height is: " + str(basicblockTest.height))
+
+
+
+
